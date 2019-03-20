35e1aae1b129a33fab7f54fa11aebd057d155331
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-03-20 16:26:38 -0400

    Adding JokeService interface

--- src/main/java/com/lopez/rafael/webjokesapp/controllers/JokesController.java
+++ src/main/java/com/lopez/rafael/webjokesapp/controllers/JokesController.java
@@ -1,25 +1,25 @@
-package com.lopez.rafael.webjokesapp.controller;
+package com.lopez.rafael.webjokesapp.controllers;
 
+import com.lopez.rafael.webjokesapp.services.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.lopez.rafael.webjokesapp.service.JokesService;
 
 @Controller
 public class JokesController {
 
-    JokesService jokesService;
+    JokeService jokeService;
 
     @Autowired
-    public JokesController(JokesService jokesService)
+    public JokesController(JokeService jokeService)
     {
-        this.jokesService = jokesService;
+        this.jokeService = jokeService;
     }
 
     @RequestMapping(path = {"/", ""})
     public String handleRequest(Model model) {
-        model.addAttribute("joke", jokesService.getRandomQuote());
+        model.addAttribute("joke", jokeService.getJoke());
         return "chucknorris";
     }
 }
---
+++ src/main/java/com/lopez/rafael/webjokesapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.lopez.rafael.webjokesapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
