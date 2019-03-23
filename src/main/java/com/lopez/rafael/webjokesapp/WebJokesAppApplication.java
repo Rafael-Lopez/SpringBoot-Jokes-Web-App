ade760a57d4eabc0d2525fdf1d6d09885222b0f4
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-03-23 11:14:26 -0400

    Adding Spring XML configuration

--- src/main/java/com/lopez/rafael/webjokesapp/WebJokesAppApplication.java
+++ src/main/java/com/lopez/rafael/webjokesapp/WebJokesAppApplication.java
@@ -2,8 +2,10 @@ package com.lopez.rafael.webjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class WebJokesAppApplication {
 
     public static void main(String[] args) {
